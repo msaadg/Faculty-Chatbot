55e02e04dbc79d9441f99426e7a202fc74aecfcb
Muhammad Saad <saad.salman11123@gmail.com>
2024-11-02 18:47:09 +0500

    added streamlit config in config_openai/config.py

--- config.py
+++ config.py
@@ -1,24 +1,44 @@
-# AZURE AI SEARCH CONFIGURATION
-from dotenv import load_dotenv
-from dotenv import dotenv_values
+# # AZURE AI SEARCH CONFIGURATION
+# from dotenv import load_dotenv
+# from dotenv import dotenv_values
 
 
-load_dotenv()
-values_env = dotenv_values(".env")
+# load_dotenv()
+# values_env = dotenv_values(".env")
 
-searchservice = values_env['searchservice']
-index = values_env['index']
-searchkey = values_env['searchkey']
-category=values_env['category']
+# searchservice = values_env['searchservice']
+# index = values_env['index']
+# searchkey = values_env['searchkey']
+# category=values_env['category']
 
-#AZURE STORAGE CONFIGURATION
-storageaccount  = values_env['storageaccount']
-container=values_env['container']
-storagekey=values_env['storagekey']
+# #AZURE STORAGE CONFIGURATION
+# storageaccount  = values_env['storageaccount']
+# container=values_env['container']
+# storagekey=values_env['storagekey']
 
-localpdfparser=values_env['localpdfparser']
-verbose=values_env['verbose']
+# localpdfparser=values_env['localpdfparser']
+# verbose=values_env['verbose']
 
-FILE_PATH = values_env['FILE_PATH']
+# FILE_PATH = values_env['FILE_PATH']
 
-formrecognizerservice=values_env['formrecognizerservice']
+# formrecognizerservice=values_env['formrecognizerservice']
+
+
+# STREAMLIT CONFIGURATION
+import streamlit as st
+
+searchservice = st.secrets["searchservice"]
+index = st.secrets["index"]
+searchkey = st.secrets["searchkey"]
+category = st.secrets["category"]
+
+# AZURE STORAGE CONFIGURATION
+storageaccount = st.secrets["storageaccount"]
+container = st.secrets["container"]
+storagekey = st.secrets["storagekey"]
+
+localpdfparser = st.secrets["localpdfparser"]
+verbose = st.secrets["verbose"]
+
+FILE_PATH = st.secrets["FILE_PATH"]
+formrecognizerservice = st.secrets["formrecognizerservice"]
--- config_openai.py
+++ config_openai.py
@@ -1,3 +1,16 @@
+# from dotenv import load_dotenv,dotenv_values
+
+
+# load_dotenv()
+# values_env_openai = dotenv_values(".env")
+
+# key = values_env_openai['key']
+# location = values_env_openai['location']
+# endpoint = values_env_openai['endpoint']
+# deployment_id_gpt4=values_env_openai['deployment_id_gpt4']  
+
+
+# STREAMLIT CONFIGURATION
 import streamlit as st
 
 key = st.secrets["key"]
